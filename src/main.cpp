58e4155beac0c010a70dfc534c75d97da5bd5689
Nya Meow <nyameowmeow@icloud.com>
2025-03-10 18:21:08 +0700

    Custom setting validation

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,15 @@
 #include "utils/customSettings.hpp"
 
 // where's my estrogen pills 
-// i need them now
-// 
+// i still couldn't find them
+// sorry km7
 
 using namespace geode::prelude;
 
 $execute {
+    // custom setting
     (void) Mod::get()->registerCustomSettingType("gaydient-colors", ColorListSetting::parse);
+
+    // setting validation (old preset system subject to removal)
+    listenForSettingChanges("preset", [](std::string value) { if (value == "Custom") { Mod::get()->setSavedValue<bool>("enable-custom", true); } else { Mod::get()->setSavedValue<bool>("enable-custom", false); }});
 }
--- src/ui/README.md
+++ src/ui/README.md
@@ -2,5 +2,6 @@
 
 ## Thanks To
 
-[TheBearodactyl](https://github.com/TheBearodactyl) for the color popup implementation
+[TheBearodactyl](https://github.com/TheBearodactyl) for the original color popup implementation
 [TheSillyDoggo](https://github.com/TheSillyDoggo) for her node dragging implementation
+[Kingminer7](https://github.com/kingminer7) for being a big help
